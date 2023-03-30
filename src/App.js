0b237d597359827527a1c956b72439602a57691a
ZaidFada231 <zaidfada01@gmail.com>
2023-03-29 20:53:01 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Typewriter from "typewriter-effect";
+import React from "react";
+import Papa from "papaparse";
+
+
+const AnotherComponent = () => {
+  return <button> my button </button>
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Typewriter
+        onInit = {(typewriter) => {
+          typewriter
+          .typeString("Good morning Mr Resturaunt")
+          .pauseFor(1000)
+          .deleteAll()
+          .typeString("We have data on the numbers of meals we currently have reserved for you.")
+          .pauseFor(1000)
+          .deleteAll()
+          .typeString("Ready to see?")
+          .start();
+        }}
+      />
+
+    <button> my button </button>
     </div>
   );
 }
