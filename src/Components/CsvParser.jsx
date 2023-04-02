48f22499887c4d4e72c5612737855045ad7ce33c
ZaidFada231 <zaidfada01@gmail.com>
2023-04-02 09:06:40 -0400

    final push

--- src/Components/CsvParser.jsx
+++ src/Components/CsvParser.jsx
@@ -1,11 +1,14 @@
 import React, {useState} from 'react';
 import Papa from "papaparse";
+import CSV_data from "./opt_ins.csv";
 import moment from 'moment';
 import './styles/CsvParser.css';
 
+
+const allowedExtensions = ["csv"];
+
 const CsvParser = () =>{
   const NewDate = moment();
-  console.log(NewDate.format('h:mm:ss'));
 
   const [parsedData, setParsedData] = useState([]);
 
@@ -27,11 +30,12 @@ const CsvParser = () =>{
 
         // Iterating data to get column name and their values
         results.data.map((d) => {
-          const date = JSON.stringify(d.meal_opt_in_created_date); //turning the time/ date element into a string
+          const date = JSON.stringify(d.meal_opt_in_created_date);  //turning the time/ date element into a string
           const [dayHour, weirdHourAdd] = date.split('.'); //spliting it up two times till wejust get the hour
           const dayAndHour = JSON.stringify(dayHour);
-          const [dat, time] = dayHour.split(' ');
-          if(NewDate.format('h:mm:ss') >= time){ //comparing the current time using moment js to the time in the csv
+          const [dateSplit, time] = dayHour.split(' ');
+          const finalTime = JSON.stringify(time);
+          if(NewDate.format('hh:mm:ss') >= time){ //comparing the current time using moment js to the time in the csv
             rowsArray.push(Object.keys(d));
             valuesArray.push(Object.values(d));
           }
@@ -59,14 +63,13 @@ const CsvParser = () =>{
         style={{ display: "block", margin: "10px auto" }}
       />
       <br />
-      <p> If you have any question on the information below, please email us at vendors@heyParkday.com </p>
       <br />
       <table class="styled-table">
         <thead>
           <tr>
-            {tableRows.map((rows, index) => { //maping through all the title names
+            {tableRows.map((rows, index) => {  //maping through all the title names
                 if(index !== 0){ // I know this isn't the cleanest bit of code, but using or statement's didn't work so I had to opt for nested if's
-                  if(index !== 1){
+                  if(index !== 1){ //they remove all the redundant/ not neccessary column nanmes such as service_id
                   if(index !== 5){
                      if(index !== 6){
                       return <th key={index}>{rows}</th>;
@@ -78,18 +81,18 @@ const CsvParser = () =>{
           </tr>
         </thead>
         <tbody>
-          {values.map((value, index) => {
+          {values.map((value, index) => {  //maping through all the title names
             return (
               <tr key={index}>
-                {value.map((val, i) => { //maping through all the title names
+                {value.map((val, i) => {
                      if(i !== 0){ // I know this isn't the cleanest bit of code, but using or statement's didn't work so I had to opt for nested if's
-                       if(i !== 1){ //they remove all the redundant/ not neccessary information such as service_id
+                      if(i !== 1){  //they remove all the redundant/ not neccessary information such as service_id
                        if(i !== 5){
                          if(i !== 6){
                           return <td key={i}>{val}</td>;
                          }
                        }
-                     }
+                    }
                    }
                 })}
               </tr>
